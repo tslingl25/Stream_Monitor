ce45208dc15a4c953fcf42a2486ef3045a277a8c
nmckee78 <nmckee78@charlottesvilleschools.org>
2023-11-16 09:45:12 -0500

    Comments

--- .vscode/turbidity_code.py
+++ .vscode/turbidity_code.py
@@ -3,13 +3,13 @@ from analogio import AnalogIn
 import board
 import time
 
-analog_in = AnalogIn(board.A0)
-def get_voltage(pin):
-    return (pin.value * 3.3) / 65536
-x = get_voltage(analog_in)
-turbidity = (-1120.4*(x**2) + 5742.3*(x) - 4352.9)
+analog_in = AnalogIn(board.A0) # Defines GP0 as the analog pin we are using
+def get_voltage(pin): # Sets up a function to measure the voltage in the GPO pin
+    return (pin.value * 3.3) / 65536 # equation to measure the voltage value
+x = get_voltage(analog_in) # Defines x as the variable used in the turbidity equation to represent the voltage
+turbidity = (-1120.4*(x**2) + 5742.3*(x) - 4352.9) # turbidity equation where y is turbidity and x is voltage, allows turbidity to be graphed
 while True:
-    print((turbidity))
+    print((turbidity)) # prints the turbidity value
     time.sleep(10)
     
 
