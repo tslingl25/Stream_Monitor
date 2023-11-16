d4a139abfa81a5a186d558e2648dc19c2bb945eb
nmckee78 <nmckee78@charlottesvilleschools.org>
2023-11-16 09:40:27 -0500

    Turbidity code done

--- .vscode/turbidity_code.py
+++ .vscode/turbidity_code.py
@@ -4,10 +4,12 @@ import board
 import time
 
 analog_in = AnalogIn(board.A0)
-
 def get_voltage(pin):
     return (pin.value * 3.3) / 65536
-
+x = get_voltage(analog_in)
+turbidity = (-1120.4*(x**2) + 5742.3*(x) - 4352.9)
 while True:
-    print((get_voltage(analog_in),))
-    time.sleep(0.1)
+    print((turbidity))
+    time.sleep(10)
+    
+
